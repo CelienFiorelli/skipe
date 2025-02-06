eb1f8b71040d38024f1e941e0977f846b9d93ec6
CelienFio <celien.fiorelli@ynov.com>
2025-02-06 09:48:48 +0100

    init laravel

--- api/api/Extensions/StringExtension.cs
+++
@@ -1,8 +0,0 @@
-﻿using System.Text.RegularExpressions;
-
-namespace Api.Extensions;
-
-public static class StringExtension
-{
-    public static string XSS(this string _valeur) => Regex.Replace(_valeur, "<[^>]*>", "", RegexOptions.Compiled);
-}
